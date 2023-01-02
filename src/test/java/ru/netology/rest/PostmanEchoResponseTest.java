20d8f7c93f67ca2e4ee0f91426bf325157586e8b
Dmitry Bychkov <yom.4100@gmail.com>
2023-01-02 17:57:55 +0300

    success build

---
+++ src/test/java/ru/netology/rest/PostmanEchoResponseTest.java
@@ -0,0 +1,45 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoResponseTest {
+    @Test
+    public void shouldResponse200() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void shouldResponseSameBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    public void shouldResponse404() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .get("/post")
+                .then()
+                .statusCode(404)
+        ;
+    }
+}
